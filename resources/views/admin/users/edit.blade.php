df1fa39aaef1fe763024b35a0875f99d45d5241a
fifiaman93 <hafizarahman93@gmail.com>
2022-11-17 01:18:57 +0800

    Done code

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.edit') }} {{ trans('cruds.user.title_singular') }}
+    </div>
+
+    <div class="card-body">
+        <form method="POST" action="{{ route("admin.users.update", [$user->id]) }}" enctype="multipart/form-data">
+            @method('PUT')
+            @csrf
+            <div class="form-group">
+                <label class="required" for="name">{{ trans('cruds.user.fields.name') }}</label>
+                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $user->name) }}" required>
+                @if($errors->has('name'))
+                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.name_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <label class="required" for="email">{{ trans('cruds.user.fields.email') }}</label>
+                <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email" id="email" value="{{ old('email', $user->email) }}" required>
+                @if($errors->has('email'))
+                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.email_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <label class="required" for="password">{{ trans('cruds.user.fields.password') }}</label>
+                <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password" id="password">
+                @if($errors->has('password'))
+                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.password_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <label class="required" for="roles">{{ trans('cruds.user.fields.roles') }}</label>
+                <div style="padding-bottom: 4px">
+                    <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
+                    <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
+                </div>
+                <select class="form-control select2 {{ $errors->has('roles') ? 'is-invalid' : '' }}" name="roles[]" id="roles" multiple required>
+                    @foreach($roles as $id => $role)
+                        <option value="{{ $id }}" {{ (in_array($id, old('roles', [])) || $user->roles->contains($id)) ? 'selected' : '' }}>{{ $role }}</option>
+                    @endforeach
+                </select>
+                @if($errors->has('roles'))
+                    <span class="text-danger">{{ $errors->first('roles') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.roles_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <label class="required" for="ml">{{ trans('cruds.user.fields.ml') }}</label>
+                <input class="form-control {{ $errors->has('ml') ? 'is-invalid' : '' }}" type="text" name="ml" id="ml" value="{{ old('ml', $user->ml) }}" required>
+                @if($errors->has('ml'))
+                    <span class="text-danger">{{ $errors->first('ml') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.ml_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <label class="required">{{ trans('cruds.user.fields.game_rank') }}</label>
+                <select class="form-control {{ $errors->has('game_rank') ? 'is-invalid' : '' }}" name="game_rank" id="game_rank" required>
+                    <option value disabled {{ old('game_rank', null) === null ? 'selected' : '' }}>{{ trans('global.pleaseSelect') }}</option>
+                    @foreach(App\Models\User::GAME_RANK_SELECT as $key => $label)
+                        <option value="{{ $key }}" {{ old('game_rank', $user->game_rank) === (string) $key ? 'selected' : '' }}>{{ $label }}</option>
+                    @endforeach
+                </select>
+                @if($errors->has('game_rank'))
+                    <span class="text-danger">{{ $errors->first('game_rank') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.game_rank_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <label>{{ trans('cruds.user.fields.game_role') }}</label>
+                <select class="form-control {{ $errors->has('game_role') ? 'is-invalid' : '' }}" name="game_role" id="game_role">
+                    <option value disabled {{ old('game_role', null) === null ? 'selected' : '' }}>{{ trans('global.pleaseSelect') }}</option>
+                    @foreach(App\Models\User::GAME_ROLE_SELECT as $key => $label)
+                        <option value="{{ $key }}" {{ old('game_role', $user->game_role) === (string) $key ? 'selected' : '' }}>{{ $label }}</option>
+                    @endforeach
+                </select>
+                @if($errors->has('game_role'))
+                    <span class="text-danger">{{ $errors->first('game_role') }}</span>
+                @endif
+                <span class="help-block">{{ trans('cruds.user.fields.game_role_helper') }}</span>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-danger" type="submit">
+                    {{ trans('global.save') }}
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+@endsection
